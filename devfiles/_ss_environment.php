1b847ccbe09f18796f4f518e59d81f143494017e
Anselm Christophersen <ac@anselm.dk>
2016-05-30 11:08:27 +0200

    SilverStripe install

---
+++ devfiles/_ss_environment.php
@@ -0,0 +1,41 @@
+<?php
+define('SS_DATABASE_SERVER', 'localhost');
+define('SS_DATABASE_USERNAME', 'root');
+define('SS_DATABASE_PASSWORD', 'root');
+define('SS_DATABASE_NAME', 'blocksdemo');
+
+//enable this for SilverStripe 3.2+
+//define('SS_DATABASE_CLASS', 'MySQLPDODatabase');
+
+define('SS_ENVIRONMENT_TYPE', 'live');
+
+define('SS_DEFAULT_ADMIN_USERNAME', 'noreply@title.dk');
+define('SS_DEFAULT_ADMIN_PASSWORD', 'xxxx');
+
+//error log - the first line seems to be the proper one
+//you might need to create silverstripe.log though, and give it the proper permissions
+define('SS_ERROR_LOG', '../logs/silverstripe.log');
+//define('SS_ERROR_LOG', __DIR__ . '/logs/silverstripe.log');
+
+
+global $_FILE_TO_URL_MAPPING;
+$_FILE_TO_URL_MAPPING['/Users/anselm/git-repos/open-source/silverstripe-blocks-demo/public'] = 'http://blocksdemo:8888';
+
+
+
+//FOR TEST SITES:
+
+//Protect the site with basic auth
+//define('SS_USE_BASIC_AUTH', true);
+
+//Emailing
+//define('SS_SEND_ALL_EMAILS_TO', '');
+//define('SS_SEND_ALL_EMAILS_FROM', '');
+
+
+//find more configuration options here:
+//http://doc.silverstripe.org/framework/en/topics/environment-management
+//https://gist.github.com/ryanwachtl/5793212
+
+
+
